eea6166098f77733f489fb6521d6e51893a10305
RoseZhang03 <rosesarered.zhang@gmail.com>
2024-11-30 22:53:43 -0600

    changed Tools to Resources, added temporary youtube playlist

---
+++ src/app/resources/page.tsx
@@ -0,0 +1,12 @@
+export default function Tools() {
+  return (
+    <main className="min-h-screen p-8">
+      <h1 className="text-3xl font-bold mb-6">Resources</h1>
+      <p>Poker Solver: Stay Tuned!</p>
+      <p className="mt-6">Poker Lecture Videos:</p>
+      <div className="mt-6">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?si=h3vtVULrP-6NGfPi&amp;list=PL0WnRS5vmGNqcdyUBBpEbMDFOstSSxoaV" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+      </div>
+    </main>
+  );
+}
