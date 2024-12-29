660e78b85db2dc7d0d4804a517167ecc0528d417
Patrick Smith <posmith2@illinois.edu>
2024-12-29 18:18:54 -0500

    First revision of text spacing

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,23 @@ export default function About() {
   return (
     <main className="min-h-screen p-8">
       <h1 className="text-3xl font-bold mb-6">About Us</h1>
-      <p>Learn more about GTO Illini and our mission here.</p>
+        <div className="mb-6">
+          <h2 className="text-2xl font-bold">Lectures</h2>
+            <p className="mt-2">GTO Illini presents regular lectures on a variety of poker topics, ranging from the basics to deep dives into advanced concepts. Check out our events page to make sure you don't miss any, and our resources page to take a look at past lectures.</p>
+        </div>
+        <div className="mb-6">
+          <h2 className="text-2xl font-bold">Development</h2>
+            <p className="mt-2">To make poker learning as accessible as possible, GTO Illini is constantly developing new poker learning software for members to make use of. Check out our resources page to start using our tools and upping your game!</p>
+        </div>
+        <div className="mb-6">
+          <h2 className="text-2xl font-bold">Competitions</h2>
+            <p className="mt-2">Poker is difficult to study practically without experience, so we hold regular online competitions to sharpen your knowledge and test your skills!</p>
+        </div>
+        <div className="mb-6">
+          <h2 className="text-2xl font-bold">Leadership</h2>
+            <p className="mt-2"></p>
+        </div>
+
     </main>
   );
 }
