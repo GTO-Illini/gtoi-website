311d63274cc826134f7b8f5cb657d3aa4a0d7ec3
Patrick Smith <posmith2@illinois.edu>
2025-01-04 12:06:26 -0500

    Added leadership image assets to public folder and about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,38 @@
 export default function About() {
+
+  const leadership = [
+    {
+      title: "President",
+      name: "Patrick Smith",
+      image: "/leadership_photos/default.png",
+      linkedin: "https://www.linkedin.com/in/patrick-o-smith/"
+    },
+    {
+      title: "Vice President",
+      name: "Kevin Zhang",
+      image: "/leadership_photos/default.png",
+      linkedin: "https://www.linkedin.com/in/kevin-y-zhang/"
+    },
+    {
+      title: "Treasurer",
+      name: "Rose Zhang",
+      image: "/leadership_photos/default.png",
+      linkedin: "https://www.linkedin.com/in/rose-zhang-m/"
+    },
+    {
+      title: "Secretary",
+      name: "Owen Vela",
+      image: "/leadership_photos/default.png",
+      linkedin: "https://www.linkedin.com/in/owen-vela/"
+    },
+    {
+      title: "Membership Director",
+      name: "Peter Lin",
+      image: "/leadership_photos/peter_being_a_stud.jpg",
+      linkedin: "https://www.linkedin.com/in/peter-q-lin/"
+    },
+  ];
+
   return (
     <main className="min-h-screen p-8">
       <h1 className="text-3xl font-bold mb-6">About Us</h1>
@@ -16,7 +50,23 @@ export default function About() {
         </div>
         <div className="mb-6">
           <h2 className="text-2xl font-bold">Leadership</h2>
-            <p className="mt-2"></p>
+            <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 mt-4">
+              {leadership.map((leader, index) => (
+                <div key={index} className="flex flex-col items-center text-center">
+                  <img
+                    src={leader.image}
+                    alt={leader.name}
+                    loading="lazy"
+                    width="800"
+                    height="800"
+                    decoding="async"
+                    className="aspect-square w-full max-w-56 max-h-56 object-cover rounded-full mb-2" // Added styling for image
+                  />
+                  <h3 className="text-xl font-bold">{leader.title}</h3>
+                  <p className="text-l font-bold">{leader.name}</p>
+                </div>
+              ))}
+            </div>
         </div>
 
     </main>
