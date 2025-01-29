67d09d143d677b6040a27f060ed4065f5e20705c
RoseZhang03 <rosesarered.zhang@gmail.com>
2025-01-29 11:43:13 -0600

    moved merch pictures to image-assets folder, organized merch page using a map

--- src/app/merch/page.tsx
+++ src/app/merch/page.tsx
@@ -6,35 +6,40 @@ import styles from './Merch.module.css';
 
 
 export default function Merch() {
+  const merch = [
+    {
+      name: "Hoodie With Logo",
+      price: "$30",
+      image: "/image-assets/merch_photos/hoodie.jpeg",
+    },
+    {
+      name: "Pin Button With Logo",
+      price: "$4",
+      image: "/image-assets/merch_photos/pin_button.jpeg",
+    },
+    {
+      name: "Playing Cards With Logo",
+      price: "$10",
+      image: "/image-assets/merch_photos/playing_cards.jpeg",
+    },
+  ];
  return (
    <main className="min-h-screen p-8">
      <h1 className="text-4xl font-bold mb-6 flex justify-center">Merchandise</h1>
      <p className="text-xl mb-6 flex justify-center">GTO Illini merchandise purchase form will be available here soon.</p>
      <div className={"flex justify-center gap-24 mt-10"}>
-       <div className="flex flex-col items-center">
-         <Image
-           src={hoodie}
-           alt="Hoodie With Logo"
-           className={styles.merchImage}
-         />
-         <p className="mt-2 text-center">Hoodie With Logo: $30</p>
-       </div>
-       <div className="flex flex-col items-center">
-         <Image
-           src={poker_cards}
-           alt="Playing Cards With Logo"
-           className={styles.merchImage}
-         />
-         <p className="mt-2 text-center">Playing Cards With Logo: $13</p>
-       </div>
-       <div className="flex flex-col items-center">
-         <Image
-           src={pin_button}
-           alt="Pin Button With Logo"
-           className={styles.merchImage}
-         />
-         <p className="mt-2 text-center">Pin Button With Logo: $4</p>
-       </div>
+     {merch.map((item, index) => (
+                <div key={index} className="flex flex-col items-center text-center">
+                  <img
+                    src={item.image}
+                    alt={item.name}
+                    loading="lazy"
+                    decoding="async"
+                    className={styles.merchImage}
+                  />
+                  <p className="mt-2 text-center">{`${item.name}: ${item.price}`}</p>
+                </div>
+              ))}
      </div>
    </main>
  );
