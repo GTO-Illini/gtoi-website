432b6d61af57db47de076cf1cff0fcaa0f8760b6
ericy <ericyousong@gmail.com>
2026-06-09 22:03:40 -0400

    config changes for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,9 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  /* config options here */
-  ...(isProd
-    ? { output: "export" }
-    : {}
-  ),
   images: {
     unoptimized: false,
   },
-  // assetPrefix: "./",
-  // assetPrefix: "https://gtoi-website.vercel.app/",
-  // assetPrefix: "./",
 };
 
 export default nextConfig;
